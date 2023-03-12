afd298dfdba281fbe4e8043abffee625c6270d8e
John LaFata <jlafata@vmware.com>
2023-03-12 14:16:34 -0400

    generalize packaging

--- src/main/java/com/mydomainname/projectname/Application.java
+++ src/main/java/com/mydomainname/projectname/Application.java
@@ -1,4 +1,4 @@
-package com.morganstanley.projectname;
+package com.mydomainname.projectname;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/mydomainname/projectname/ApplicationTests.java
+++ src/test/java/com/mydomainname/projectname/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.morganstanley.projectname;
+package com.mydomainname.projectname;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
